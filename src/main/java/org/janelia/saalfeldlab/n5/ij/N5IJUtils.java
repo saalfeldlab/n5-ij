0fd40eb3eb3cda09e279dab4133197e2830f8234
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2018-02-02 21:23:40 -0500

    simple API to load and save N5 datasets in ImageJ

---
+++ src/main/java/org/janelia/saalfeldlab/n5/ij/N5IJUtils.java
@@ -0,0 +1,281 @@
+/**
+ * License: GPL
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.janelia.saalfeldlab.n5.ij;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+
+import org.janelia.saalfeldlab.n5.Compression;
+import org.janelia.saalfeldlab.n5.DatasetAttributes;
+import org.janelia.saalfeldlab.n5.N5Reader;
+import org.janelia.saalfeldlab.n5.N5Writer;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+
+import ij.ImagePlus;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converters;
+import net.imglib2.exception.ImgLibException;
+import net.imglib2.img.cell.LazyCellImg;
+import net.imglib2.img.imageplus.ImagePlusImg;
+import net.imglib2.img.imageplus.ImagePlusImgs;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.util.Pair;
+import net.imglib2.view.Views;
+
+/**
+ *
+ *
+ * @author Stephan Saalfeld &lt;saalfelds@janelia.hhmi.org&gt;
+ */
+public class N5IJUtils {
+
+	/**
+	 * Loads and N5 dataset into an {@link ImagePlus}.  Other than
+	 * {@link N5Utils#open(N5Reader, String)} which uses {@link LazyCellImg},
+	 * the data is actually loaded completely into memory.
+	 *
+	 * @param n5
+	 * @param dataset
+	 * @return
+	 * @throws IOException
+	 * @throws ImgLibException
+	 */
+	@SuppressWarnings( { "unchecked", "rawtypes" } )
+	public static <T extends NativeType<T> & NumericType<T>> ImagePlus load(
+			final N5Reader n5,
+			final String dataset) throws IOException, ImgLibException {
+
+		final RandomAccessibleInterval<T> rai = N5Utils.open(n5, dataset);
+		final DatasetAttributes attributes = n5.getDatasetAttributes(dataset);
+		final long[] dimensions = attributes.getDimensions();
+		final ImagePlusImg<T, ?> impImg;
+		switch (attributes.getDataType()) {
+		case UINT8:
+			impImg = (ImagePlusImg)ImagePlusImgs.unsignedBytes(dimensions);
+			break;
+		case INT8:
+			impImg = (ImagePlusImg)ImagePlusImgs.bytes(dimensions);
+			break;
+		case UINT16:
+			impImg = (ImagePlusImg)ImagePlusImgs.unsignedShorts(dimensions);
+			break;
+		case INT16:
+			impImg = (ImagePlusImg)ImagePlusImgs.shorts(dimensions);
+			break;
+		case UINT32:
+			impImg = (ImagePlusImg)ImagePlusImgs.unsignedInts(dimensions);
+			break;
+		case INT32:
+			impImg = (ImagePlusImg)ImagePlusImgs.ints(dimensions);
+			break;
+		case FLOAT32:
+			impImg = (ImagePlusImg)ImagePlusImgs.floats(dimensions);
+			break;
+		default:
+			System.err.println("Data type " + attributes.getDataType() + " not supported in ImageJ.");
+			return null;
+		}
+
+		for (final Pair<T, T> pair : Views.flatIterable(Views.interval(Views.pair(rai, impImg), rai)))
+			pair.getB().set(pair.getA());
+
+		return impImg.getImagePlus();
+	}
+
+
+	/**
+	 * Save an {@link ImagePlus} as an N5 dataset.
+	 *
+	 * @param imp
+	 * @param n5
+	 * @param datasetName
+	 * @param blockSize
+	 * @param compression
+	 * @throws IOException
+	 */
+	public static void save(
+			final ImagePlus imp,
+			final N5Writer n5,
+			final String datasetName,
+			final int[] blockSize,
+			final Compression compression) throws IOException {
+
+		final ImagePlusImg<ARGBType, ?> rai = ImagePlusImgs.from(imp);
+
+		N5Utils.save(
+				rai,
+				n5,
+				datasetName,
+				blockSize,
+				compression);
+	}
+
+	/**
+	 * Save and {@link ImagePlus} as an N5 dataset.  Parallelizes export using
+	 * an {@link ExecutorService}.
+	 *
+	 * @param imp
+	 * @param n5
+	 * @param datasetName
+	 * @param blockSize
+	 * @param compression
+	 * @param exec
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	public static void save(
+			final ImagePlus imp,
+			final N5Writer n5,
+			final String datasetName,
+			final int[] blockSize,
+			final Compression compression,
+			final ExecutorService exec) throws IOException, InterruptedException, ExecutionException {
+
+		final ImagePlusImg<ARGBType, ?> rai = ImagePlusImgs.from(imp);
+
+		N5Utils.save(
+				rai,
+				n5,
+				datasetName,
+				blockSize,
+				compression,
+				exec);
+	}
+
+	/**
+	 * Save an ARGB image that contains gray scale data and uses inequality of
+	 * the three colors as a mask channel into an N5 group with two uint8 datasets
+	 * ('gray' and 'mask).  Mask is 1 where all color channels are equal and 0
+	 * everywhere else.
+	 *
+	 * @param imp argb image
+	 * @param n5
+	 * @param groupName
+	 * @param blockSize
+	 * @param compression
+	 * @throws IOException
+	 */
+	public static void saveMaskedUnsignedByte(
+			final ImagePlus imp,
+			final N5Writer n5,
+			final String groupName,
+			final int[] blockSize,
+			final Compression compression) throws IOException {
+
+		final ImagePlusImg<ARGBType, ?> rai = ImagePlusImgs.from(imp);
+
+		final RandomAccessibleInterval<UnsignedByteType> mask = Converters.convert(
+				(RandomAccessibleInterval<ARGBType>)rai,
+				(argb, uint8) -> {
+					final int argbValue = argb.get();
+					final int r = ARGBType.red(argbValue);
+					final int g = ARGBType.green(argbValue);
+					final int b = ARGBType.blue(argbValue);
+					if (r == g && g == b)
+						uint8.set(1);
+					else
+						uint8.set(0);
+				},
+				new UnsignedByteType());
+
+		final RandomAccessibleInterval<UnsignedByteType> gray = Converters.convert(
+				(RandomAccessibleInterval<ARGBType>)rai,
+				(argb, uint8) -> {
+					uint8.set(ARGBType.red(argb.get()));
+				},
+				new UnsignedByteType());
+
+		n5.createGroup(groupName);
+		N5Utils.save(
+				mask,
+				n5, groupName + "/mask",
+				blockSize,
+				compression);
+		N5Utils.save(
+				gray,
+				n5, groupName + "/gray",
+				blockSize,
+				compression);
+	}
+
+	/**
+	 * Save an ARGB image that contains gray scale data and uses inequality of
+	 * the three colors as a mask channel into an N5 group with two uint8 datasets
+	 * ('gray' and 'mask).  Mask is 1 where all color channels are equal and 0
+	 * everywhere else.  Parallelizes export using an {@link ExecutorService}.
+	 *
+	 * @param imp ARGB image
+	 * @param n5
+	 * @param groupName
+	 * @param blockSize
+	 * @param compression
+	 * @param exec
+	 * @throws IOException
+	 * @throws InterruptedException
+	 * @throws ExecutionException
+	 */
+	public static void saveMaskedUnsignedByte(
+			final ImagePlus imp,
+			final N5Writer n5,
+			final String groupName,
+			final int[] blockSize,
+			final Compression compression,
+			final ExecutorService exec) throws IOException, InterruptedException, ExecutionException {
+
+		final ImagePlusImg<ARGBType, ?> rai = ImagePlusImgs.from(imp);
+
+		final RandomAccessibleInterval<UnsignedByteType> mask = Converters.convert(
+				(RandomAccessibleInterval<ARGBType>)rai,
+				(argb, uint8) -> {
+					final int argbValue = argb.get();
+					final int r = ARGBType.red(argbValue);
+					final int g = ARGBType.green(argbValue);
+					final int b = ARGBType.blue(argbValue);
+					if (r == g && g == b)
+						uint8.set(1);
+					else
+						uint8.set(0);
+				},
+				new UnsignedByteType());
+
+		final RandomAccessibleInterval<UnsignedByteType> gray = Converters.convert(
+				(RandomAccessibleInterval<ARGBType>)rai,
+				(argb, uint8) -> {
+					uint8.set(ARGBType.red(argb.get()));
+				},
+				new UnsignedByteType());
+
+		n5.createGroup(groupName);
+		N5Utils.save(
+				mask,
+				n5, groupName + "/mask",
+				blockSize,
+				compression,
+				exec);
+		N5Utils.save(
+				gray,
+				n5, groupName + "/gray",
+				blockSize,
+				compression,
+				exec);
+	}
+}
