6576892a8c569a7a0b3003c81d199a31b287f6fe
John Bogovic <bogovicj@janelia.hhmi.org>
2020-10-12 14:38:59 -0400

    gcloud fetch SA creds from resource

--- src/main/java/org/janelia/saalfeldlab/n5/demos/TestGCloudServiceAcctAuth.java
+++ src/main/java/org/janelia/saalfeldlab/n5/demos/TestGCloudServiceAcctAuth.java
@@ -1,6 +1,10 @@
 package org.janelia.saalfeldlab.n5.demos;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Blob;
@@ -10,18 +14,33 @@ import com.google.cloud.storage.StorageOptions;
 
 public class TestGCloudServiceAcctAuth
 {
+	
+	private Storage storage;
+
+	public TestGCloudServiceAcctAuth()
+	{
+		InputStream credsStream = this.getClass().getResourceAsStream("n5testproject-65b45b8b12bd.json");
+		GoogleCredentials credentials;
+		try {
+			credentials = GoogleCredentials.fromStream( credsStream );
+			storage = StorageOptions.newBuilder().setCredentials( credentials ).build().getService();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main( String[] args ) throws Exception
 	{
-//		/home/john/googleCloud/n5testproject-65b45b8b12bd.json
-		final String jsonPath = args[0];
-		GoogleCredentials credentials = GoogleCredentials.fromStream( new FileInputStream( jsonPath ) );
-		Storage storage = StorageOptions.newBuilder().setCredentials( credentials ).build().getService();
-
+		new TestGCloudServiceAcctAuth().runTest();
+	}
+	
+	public void runTest()
+	{
 		System.out.println( "Reading.." );
 		Blob b = storage.get( BlobId.of( "example_multi-n5_bucket", "position.n5/attributes.json" ) );
 		System.out.println( "Blob:" + b );
-		
 	}
 
 
