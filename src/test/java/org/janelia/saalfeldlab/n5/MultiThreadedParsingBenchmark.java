b5bde91ab4112a4e2f44c9cf32b8972fa38a50e9
bogovicj <bogovicj@janelia.hhmi.org>
2021-03-24 17:55:28 -0400

    update parsing benchmark

--- src/test/java/org/janelia/saalfeldlab/n5/MultiThreadedParsingBenchmark.java
+++ src/test/java/org/janelia/saalfeldlab/n5/MultiThreadedParsingBenchmark.java
@@ -1,18 +1,14 @@
 package org.janelia.saalfeldlab.n5;
 
 import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
 
 import org.janelia.saalfeldlab.n5.N5DatasetDiscoverer;
 import org.janelia.saalfeldlab.n5.N5Reader;
 import org.janelia.saalfeldlab.n5.N5TreeNode;
 import org.janelia.saalfeldlab.n5.dataaccess.DataAccessException;
-import org.janelia.saalfeldlab.n5.dataaccess.DataAccessFactory;
-import org.janelia.saalfeldlab.n5.dataaccess.DataAccessType;
+import org.janelia.saalfeldlab.n5.ij.N5Factory;
 import org.janelia.saalfeldlab.n5.ij.N5Importer;
 
 public class MultiThreadedParsingBenchmark
@@ -28,31 +24,11 @@ public class MultiThreadedParsingBenchmark
         final String n5RootPath = args[ 0 ];
         final String n5Dataset = args[ 1 ];
 
-		N5Reader n5 = new DataAccessFactory( DataAccessType.AMAZON_S3, n5RootPath ).createN5Reader( n5RootPath );
-		N5TreeNode root = new N5TreeNode( n5Dataset, false );
+		N5Reader n5 = new N5Factory().openReader( n5RootPath );
 
 		long start = System.currentTimeMillis();
 		System.out.println( "discover" );
-		LinkedBlockingQueue< Future< N5TreeNode > > parseFutures = discoverer.discoverThreads( n5, root );
-		try
-		{
-			System.out.println( executor.isShutdown() );
-			System.out.println( executor.isTerminated() );
-			while( !parseFutures.isEmpty())
-			{
-				parseFutures.poll().get();
-				//System.out.println("job done, still have " + parseFutures.size() );
-			}
-			System.out.println( "All jobs done" );
-		}
-		catch ( InterruptedException e )
-		{
-			e.printStackTrace();
-		}
-		catch ( ExecutionException e )
-		{
-			e.printStackTrace();
-		}
+		final N5TreeNode root = discoverer.discoverRecursive( n5, n5Dataset );
 		
 		long end = System.currentTimeMillis();
 
